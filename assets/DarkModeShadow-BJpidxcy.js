8033b977f25315c834254f6ab6f377b3adceca44
Mohammad <mohammad1391akbari0@gmail.com>
2026-06-04 17:26:37 +0330

    Updates

--- assets/DarkModeShadow-BJpidxcy.js
+++
@@ -1,1 +0,0 @@
-import{O as r}from"./index-Bjs9JPGG.js";function e(){const{isDarkMode:a}=r();return{darkModeShadow:a?"0 0 30px rgba(0, 200, 220, 0.3), 0 0 15px rgba(0, 242, 255, 0.3), 0 0 5px rgba(0, 180, 216, 0.5)":"0 3px 8px rgba(0,0,0,0.2), 0 2px 4px rgba(0,0,0,0.6)",isDarkMode:a}}export{e as u};
